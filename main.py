d5f448bfc86f281135901b324c33c61e0d4f338c
DarkOutcast6 <109392473+DarkOutcast6@users.noreply.github.com>
2023-09-01 23:11:59 +0000

    BETA Release 1

--- main.py
+++ main.py
@@ -1,24 +1,84 @@
+# Setup for main.py
 from flask import Flask, redirect, render_template, request
 import pycaching
 
-# geocaching = pycaching.login("user", "pass")
-
 app = Flask(__name__)
 
+# Predefined vars
+geocaching = None
+cacheWP = None
 
-# @app.route('/login', methods=["GET"])
-# def loginForm():
-#  return render_template("login.html")
+# logged in?
+if geocaching == None:
+  loggedIn = False
+else:
+  loggedIn = True
 
-# @app.route('/', methods=["POST"])
-# def processForm():
-#   form = request.form
-#   print(form)
-
-# if __name__ == '__main__':
-#     app.run('0.0.0.0', 81)
-
+# Error Function
 def error(type):
   if type == "loginNotFound":
-    return render_template('login.html', error="Login Error")
-    return redirect('/login')
+    return render_template('login.html', error="Incorrect username or password!")
+
+# Logged In Var
+@app.context_processor
+def contextProcessor():
+  return dict(loggedIn=loggedIn)
+
+# App Routes
+@app.route("/")
+def index():
+  return render_template("index.html")
+
+@app.route("/home")
+def hello():
+  return render_template("home.html")
+  
+@app.route("/about")
+def about():
+  return render_template("about.html")
+  
+@app.route("/search")
+def search():
+  return render_template("search.html")
+
+@app.route("/cache/<cacheWP>")
+def cache(cacheWP):
+  cache = geocaching.get_cache(wp=cacheWP)
+  return render_template("cache.html", cacheAuthor = cache.author, cacheDescription = cache.description, cacheTerrain = cache.terrain, cacheDifficulty = cache.difficulty)
+
+@app.route("/login")
+def login():
+  return render_template("login.html")
+
+# Login Form
+@app.route('/login', methods=["POST"])
+def loginForm():
+  global geocaching
+  global loggedIn
+  form = request.form
+  try:
+    geocaching = pycaching.login(form["username"], form["password"])
+    loggedIn = True
+    return redirect("/home")
+  except:
+    return error("loginNotFound")
+
+# Search
+@app.route("/search", methods=["POST"])
+def searchForm():
+  global cacheWP
+  form = request.form
+  if True:#form["search-caches"][1] == "G" and form["search-caches"][2] == "C":
+    # try:
+    cacheWP = form["search-caches"]
+    return redirect(f"/cache/{cacheWP}")
+    # except:
+    #   raise ValueError('Invalid GUID')
+    #   return render_template("search.html")
+  else:
+    raise ValueError('No results')
+    return render_template("search.html")
+    
+# Run Program
+if __name__ == '__main__':
+    app.run('0.0.0.0', 81)
