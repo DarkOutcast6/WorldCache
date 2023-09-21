c8c3577e6dd3e079c5a90c5c44e6e90bae962e99
DarkOutcast6 <109392473+DarkOutcast6@users.noreply.github.com>
2023-09-21 00:02:42 +0000

    BETA Release 4

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # Setup for main.py
 from flask import Flask, redirect, render_template, request
 from replit import db
-import pycaching, os
+import pycaching, os, folium, base64, re
 
 app = Flask(__name__)
 
@@ -22,6 +22,32 @@ else:
 def error(type):
   if type == "loginNotFound":
     return render_template('login.html', error="Incorrect username or password!")
+
+# Functions
+def svg_to_dataurl(path_to_svg):
+  with open(path_to_svg, 'r') as f:
+    svg = f.read()
+  return "data:image/svg+xml;base64," + base64.b64encode(svg.encode('utf-8')).decode('utf-8')
+  
+def dms2dd(degrees, minutes, seconds, direction):
+    dd = float(degrees) + float(minutes)/60 + float(seconds)/(60*60);
+    if direction == 'S' or direction == 'W':
+        dd *= -1
+    return dd;
+
+def dd2dms(deg):
+    d = int(deg)
+    md = abs(deg - d) * 60
+    m = int(md)
+    sd = (md - m) * 60
+    return [d, m, sd]
+
+def parse_dms(dms):
+    parts = re.split('[^\d\w]+', dms)
+    lat = dms2dd(parts[0], parts[1], parts[2], parts[3])
+    lng = dms2dd(parts[4], parts[5], parts[6], parts[7])
+
+    return (lat, lng)
 
 # Logged In Var
 @app.context_processor
@@ -58,10 +84,6 @@ def hello():
 def about():
   return render_template("about.html")
   
-@app.route("/search")
-def search():
-  return render_template("search.html")
-
 @app.route("/search/<user_input>")
 def searching(user_input):
   try:
@@ -77,7 +99,7 @@ def searching(user_input):
 @app.route("/cache/<cacheWP>")
 def cache(cacheWP):
   cache = geocaching.get_cache(wp=cacheWP)
-  
+  # Get logs
   def to_dict(log):
     return {
       name: str(getattr(log, name, 'null'))
@@ -85,9 +107,80 @@ def cache(cacheWP):
     }
 
   logs = [to_dict(log) for log in cache.load_logbook()]
-  # logCount = int(cache.log_counts.get("<Type.owner_maintenance: '46'>")) + int(cache.log_counts.get("<Type.found_it: '2'>"))
+
+  # Coordinate Fixing
+  cache_loc = str(cache.location)
+  negatives = {"S":False, "W":False}
+  cache_loc = cache_loc.replace("N","")
+  if "S" in cache_loc:
+    negatives["S"] = True
+    cache_loc = cache_loc.replace("S","")
+  cache_loc = cache_loc.replace("E","")
+  if "W" in cache_loc:
+    negatives["W"] = True
+    cache_loc = cache_loc.replace("S","")
+
+  cache_loc = cache_loc.replace("d","")
+  cache_loc = cache_loc.replace("s","")
+  cache_loc = cache_loc.replace("m","")
+  cache_loc = [str(parse_dms(cache_loc)[0]),str(parse_dms(cache_loc)[1])]
+
+  if negatives.get("S"):
+    cache_loc = ["-" + cache_loc[0],cache_loc[1]]
+
+  if negatives.get("W"):
+    cache_loc = [cache_loc[0],"-" + cache_loc[1]]
+
+  del negatives
+  m = folium.Map(location=cache_loc, zoom_start=12, tiles=folium.TileLayer("OpenStreetMap", show=False, name="OpenStreet Map"))
+
+  # Layer Control
+  # folium.TileLayer().add_to(m)
+  folium.TileLayer("Stamen Terrain", show=False, name="Stamen Terrain").add_to(m)
+  folium.TileLayer("Cartodb Positron", show=False, name="Cartodb Positron", attr='contributors &copy; <a href="https://carto.com/attributions">CARTO</a>').add_to(m)
+  folium.TileLayer("Esri.WorldImagery", show=False, name="Sattelite", attr='contributors &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community').add_to(m)
+  folium.LayerControl(position = "bottomright").add_to(m)
   
-  return render_template("cache.html", cacheName = cache.name, cacheWP = cache.wp, cacheAuthor = cache.author, cacheDescription = cache.description, cacheTerrain = cache.terrain, cacheDifficulty = cache.difficulty, cacheType = str(cache.type).split(".")[1], logs = logs, logCount = len(logs))
+  popup_content = """
+    <div id="custom-popup" style="display: none;">
+        <h2>Custom Popup</h2>
+        <p>This is a custom div element that shows up when you click the marker.</p>
+    </div>
+  """
+
+  try:
+    custom_icon = folium.CustomIcon(icon_image=svg_to_dataurl(f'static/imgs/type_{str(cache.type).split(".")[1]}.svg'), icon_size=(30,30))
+    marker = folium.Marker(location=cache_loc, icon=custom_icon)
+    marker.add_to(m)
+  except:
+    folium.Marker(location=cache_loc, icon=folium.Icon(color="green")).add_to(m)
+
+  js_code = """
+    <script>
+    var marker = L.DomUtil.get("custom-popup");
+
+    marker.addEventListener("click", function() {
+        var popup = L.DomUtil.get("custom-popup");
+        if (popup.style.display === "none") {
+            popup.style.display = "block";
+        } else {
+            popup.style.display = "none";
+        }
+    });
+    </script>
+  """
+
+  m.get_root().html.add_child(folium.Element(js_code))
+
+  popup_content = """
+    <div id="custom-popup" style="display: none;">
+        <h2>{}</h2>
+        <p>{}</p>
+    </div>
+  """.format(cache.name, cache.description)
+
+  map_html = m._repr_html_()
+  return render_template("cache.html", cacheName = cache.name, cacheWP = cache.wp, cacheAuthor = cache.author, cacheDescription = cache.description_html, cacheTerrain = cache.terrain, cacheDifficulty = cache.difficulty, cacheHint = cache.hint, cacheType = str(cache.type).split(".")[1], cacheSize = str(cache.size).split(".")[1].replace("_"," ").capitalize(), logs = logs, logCount = len(logs), map_html = map_html, popup_content = popup_content)
 
 @app.route("/cache/<cacheWP>/log")
 def logCache(cacheWP):
@@ -112,7 +205,7 @@ def loginForm():
     return error("loginNotFound")
 
 # Search
-@app.route("/search", methods=["POST"])
+@app.route("/home", methods=["POST"])
 def searchForm():
   global cacheWP
   form = request.form
@@ -158,6 +251,7 @@ def searchingForm(user_input):
       except:
         user_input = "no-results"
         return redirect(f"/search/{user_input}")
+        
 # Run Program
 if __name__ == '__main__':
     app.run('0.0.0.0', 81)
